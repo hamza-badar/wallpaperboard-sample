55babe0d4e7df163284e3ef31063453eb477b664
hamza-badar <hamzabadar05@gmail.com>
2018-05-12 11:53:41 +0000

    enable firebase notific - special thanks to @ishubhamsingh for his referral

---
+++ app/src/main/java/com/dm/wallpaper/board/wallzplz/activities/MyJobService.java
@@ -0,0 +1,27 @@
+package com.hamzabadar.wallzplz.activities;
+
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+/**
+ * Picked by hamza-badar on 5/12/18.
+ */
+
+public class MyJobService extends JobService {
+
+    private static final String TAG = "MyJobService";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "Performing long running task in scheduled job");
+        // TODO(developer): add long running task here.
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
